cc9a0a0115f9927c802f0ce5e5ab0922e46459e8
Vladislav Elizarov <velizarovdev@gmail.com>
2021-11-16 05:53:25 +0300

    Update todo-list-item.js. All tasks that are done mark gray color with line-through.  Text of important tasks become bolder. I used variables for collecting classes and several of <if> constructions.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,13 +3,36 @@ import React from 'react'
 import "./todo-list-item.css"
 
 const TodoListItem = (props) => {
+    let classNamesForLabel = ""
+    let classNamesForBtn = "btn btn-sm"
+    let disabled = ""
+    const classNamesForBtnLenghtBefore = classNamesForBtn.length
+    
+    if (props.important) {
+        classNamesForLabel += " important"
+        classNamesForBtn += " btn-primary"
+    }
+
+    if (props.done) {
+        classNamesForLabel += " done"
+        classNamesForBtn += " btn-outline-success hide"
+        disabled = "disabled"
+    }
+
+    if (classNamesForBtn.length === classNamesForBtnLenghtBefore) {
+        classNamesForBtn += " btn-outline-secondary"
+    }
+
     return (
         <span className="todo-list-item">
-            <span>{props.label}</span>
+            <span className={classNamesForLabel}>
+                {props.label}
+            </span>
 
             <button
                 type="button"
-                className="btn btn-sm btn-outline-secondary">
+                className={classNamesForBtn}
+                disabled={disabled}>
                 <i className="bi bi-exclamation"></i>
             </button>
             
